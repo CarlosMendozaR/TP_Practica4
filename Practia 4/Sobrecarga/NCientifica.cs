d92fdc8a5088d58c0d92429ad600d4ee3a3b7fd6
Carlos <halomau@hotmail.com>
2018-08-31 15:15:43 -0500

    Se terminó la actividad 2

---
+++ Practia 4/Sobrecarga/NCientifica.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace Sobrecarga
+{
+    class NCientifica
+    {
+        private int coeficiente;
+        private int exponente;
+
+        public NCientifica (int coeficiente, int exponente)
+        {
+            this.coeficiente = coeficiente;
+            this.exponente = exponente;
+        }
+        
+        public static NCientifica operator *(NCientifica n1, NCientifica n2)
+        {
+            NCientifica n3 = new NCientifica(n1.coeficiente * n2.coeficiente, n1.exponente+n2.exponente);
+            return n3;
+        }
+        public static NCientifica operator ^(NCientifica n1, byte n2)
+        {
+            NCientifica n = new NCientifica(Convert.ToInt32(Math.Pow(n1.coeficiente, n2)),n1.exponente*n2);
+            return n;
+        }
+
+
+        public override string ToString()
+        {
+            return String.Format("{0}E{1}",coeficiente,exponente);
+        }
+        
+    }
+}
--- Practia 4/Sobrecarga/Program.cs
+++ Practia 4/Sobrecarga/Program.cs
@@ -6,6 +6,32 @@ namespace Sobrecarga
     {
         static void Main()
         {
+            #region ACTIVIDAD 2
+            Console.WriteLine("**Todos los números en base 10**");
+            Console.WriteLine("Ingresa el exponente al que deseas elevar tres números");
+            byte exp = byte.Parse(Console.ReadLine());
+
+            NCientifica n1 = new NCientifica(3, 7);
+            NCientifica n2 = new NCientifica(32, -1);
+            NCientifica n3 = new NCientifica(2, 3);
+            NCientifica n4 = new NCientifica(11, 9);
+
+            NCientifica n5 = n1 * n2;
+            NCientifica n6 = n1 ^ exp;
+            Console.WriteLine("\nEjemplo uno:\n{0} * {1} = {2}", n1, n2, n5);
+            Console.WriteLine("{0} ^ {1} = {2}", n1, exp, n6);
+
+            NCientifica n7 = n3 * n4;
+            NCientifica n8 = n3 ^ exp;
+            Console.WriteLine("\nEjemplo dos:\n{0} * {1} = {2}", n3, n4, n7);
+            Console.WriteLine("{0} ^ {1} = {2}", n3, exp, n8);
+
+            NCientifica n9 = n8 * n3;
+            NCientifica n0 = n8 ^ exp;
+            Console.WriteLine("\nEjemplo dos:\n{0} * {1} = {2}", n8, n3, n9);
+            Console.WriteLine("{0} ^ {1} = {2}", n8, exp, n0);
+            #endregion
+
 
         }
     }
